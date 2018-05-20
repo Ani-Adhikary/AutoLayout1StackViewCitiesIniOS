efde096116067b4ae6a701beedd649ed955b0630
Ani Adhikary <aniadhikary@Anis-MacBook-Air.local>
2018-05-21 01:42:17 +0530

    First commit

---
+++ StackViewCitiesDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SV2
+//
+//  Created by Ani Adhikary on 10/04/18.
+//  Copyright © 2018 Ani Adhikary. All rights reserved.
+//
+//  https://www.appcoda.com/stack-views-intro/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
